700201e02590d5149117148c18fad79a7fed8f66
rostyslav-devico <nordik.commerce@gmail.com>
2025-01-28 17:33:18 +0200

    404-page & main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,273 @@
-export default function Home() {
+import React from "react";
+import "./css/page.css";
+
+const App = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <h1>HOME</h1>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a>footer</a>
-      </footer>
+    <div id="kapsa">
+
+      <div id="ust">
+        <h4 id="baslik">PERSONAL CODE EDITOR</h4>
+        <div id="islem">
+          <div id="y"></div>
+          <div id="s"></div>
+          <div id="k"></div>
+        </div>
+        <div className="temizle"></div>
+      </div>
+
+      <div id="orta">
+
+        <div id="no">
+          <span className="number">1</span>
+          <span className="number">2</span>
+          <span className="number">3</span>
+          <span className="number">4</span>
+          <span className="number">5</span>
+          <span className="number">6</span>
+          <span className="number">7</span>
+          <span className="number">8</span>
+          <span className="number">9</span>
+          <span className="number">10</span>
+          <span className="number">11</span>
+          <span className="number">12</span>
+          <span className="number">13</span>
+          <span className="number">14</span>
+          <span className="number">15</span>
+          <span className="number">16</span>
+          <span className="number">17</span>
+          <span className="number">18</span>
+          <span className="number">19</span>
+          <span className="number">20</span>
+          <span className="number">21</span>
+          <span className="number">22</span>
+          <span className="number">23</span>
+          <span className="number">24</span>
+          <span className="number">25</span>
+          <span className="number">26</span>
+          <span className="number">27</span>
+          <span className="number">28</span>
+          <span className="number">29</span>
+          <span className="number">30</span>
+          <span className="number">31</span>
+          <span className="number">32</span>
+          <span className="number">33</span>
+          <span className="number">34</span>
+          <span className="number">35</span>
+          <span className="number">36</span>
+          <span className="number">37</span>
+          <span className="number">38</span>
+          <span className="number">39</span>
+          <span className="number">40</span>
+          <span className="number">41</span>
+          <span className="number">42</span>
+          <span className="number">43</span>
+          <span className="number">44</span>
+          <span className="number">45</span>
+          <span className="number">46</span>
+          <span className="number">47</span>
+          <span className="number">48</span>
+          <span className="number">49</span>
+          <span className="number">50</span>
+          <span className="number">51</span>
+          <span className="number">52</span>
+          <span className="number">53</span>
+          <span className="number">54</span>
+          <span className="number">55</span>
+          <span className="number">56</span>
+          <span className="number">57</span>
+          <span className="number">58</span>
+          <span className="number">59</span>
+        </div>
+
+        <div id="satir">
+
+          <span className="yorum">/*<br />&nbsp;&nbsp;&nbsp; Here Is My Profile -&gt; <a href="https://codepen.io/codery">Rostislav Zavodianyi</a> <br />*/</span>
+          <br />
+          <span className="cl">.Persona&nbsp;</span>
+          <span className="beyaz">{`{`}</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;name</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;Rostislav Zavodianyi</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;year</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;21</span><span className="beyaz">;</span>
+          <br />
+          <span className="beyaz">{`}`}</span>
+          <br />
+          <br />
+
+          <span className="cl">.Education&nbsp;</span>
+          <span className="beyaz">{`{`}</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;City</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;Khmelnitsky</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;College</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;"Khmelnitsky Polytechnical College"</span><span className="beyaz">;</span>
+          <span className="yorum">&nbsp;&nbsp;&nbsp;/* Bachelor of Programming Engineering, Computer Software Engineering */</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;University</span> <span className="beyaz">:</span>
+          <span className="ozellik text-[#D6FF00]">&nbsp;"Khmelnitsky National University"</span><span className="beyaz">;</span>
+          <span className="yorum">&nbsp;&nbsp;&nbsp;/* Master of Computer Science, Computer Science */</span>
+          
+
+          <span className="beyaz">{`}`}</span>
+          <br />
+          <br />
+
+          <span className="cl">.Languages&nbsp;</span>
+          <span className="beyaz">{`{`}</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;HTML5</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;CSS</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;JS(ES5, ES6+)</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="beyaz">{`}`}</span>
+          <br />
+          <br />
+
+          <span className="cl">.Technologies&nbsp;</span>
+          <span className="beyaz">{`{`}</span>
+          <br />          
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Shopify</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Bigcommerce</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;React.js</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;4 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;jQuery</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;5 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;SASS</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;5 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;LESS</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;5 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Stencil</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Handlebars.js</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Liquid</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;NPM</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Gulp</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;6 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Webpack</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;3 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Tailwind</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;4 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;Bootstrap 3</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;3 year</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="beyaz">{`}`}</span>
+          <br />
+          <br />
+
+          <span className="cl">.Language&nbsp;</span>
+          <span className="beyaz">{`{`}</span>
+          <br />
+
+          <span className="cl">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&._Ukrainian&nbsp;</span>
+          <span className="beyaz">{`{`}</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Spoken-level</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;"Native language"</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Written-level</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;"Presentation proficient"</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="beyaz">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`}`}</span>
+          <br />
+          <br />
+
+          <span className="cl">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&._English&nbsp;</span>
+          <span className="beyaz">{`{`}</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Spoken-level</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;"Upper Intermediate"</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Written-level</span> <span className="beyaz">:</span>
+          <span className="ozellik">&nbsp;"Intermediate+"</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="beyaz">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`}`}</span>
+          <br />
+
+          <span className="beyaz">{`}`}</span>
+          <br />
+          <br />
+
+          <span className="id">#some-info</span>
+          <br />
+          <span className="beyaz">{`{`}</span>
+          <br />
+
+          <span className="secici">&nbsp;&nbsp;&nbsp;content</span> <span className="beyaz">:</span>
+          <span className="ozellik text-[#D6FF00]">&nbsp;"I am a self-motivated team player, goal-oriented,</span><br/>
+          <span className="ozellik text-[#D6FF00]">&nbsp;&nbsp;&nbsp;responsible, and friendly. Quickly learn new technologies.</span><br/>
+          <span className="ozellik text-[#D6FF00]">&nbsp;&nbsp;&nbsp;In spare time try to update skills on interesting topics."</span><span className="beyaz">;</span>
+          <br />
+
+          <span className="beyaz">{`}`}</span>
+          <br />
+
+        </div>
+
+        <div className="temizle"></div>
+      </div>
+
+      <div id="alt">
+        &copy; <a href="https://codepen.io/codery">Rostislav Zavodianyi</a>
+      </div>
+
     </div>
   );
-}
+};
+
+export default App;
