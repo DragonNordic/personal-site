e3f668982f9efe47b36b503b3573deb3b7960f4a
rostyslav-devico <nordik.commerce@gmail.com>
2025-01-30 12:21:49 +0200

    contact-pop-up & style fix

--- app/contact-pop-up.jsx
+++ app/contact-pop-up.jsx
@@ -18,16 +18,16 @@ const Footer = () => {
 
   return (
     <div>
-      <div className="bg-green-100 h-8 px-4 flex items-center justify-between text-sm rounded-b-lg text-green-800">
+      <div className="bg-[#F3F3F1] h-8 px-4 flex items-center justify-between text-sm rounded-b-lg text-[#282828]">
         <a
           href="#"
-          className="border-b border-dotted text-green-800 cursor-pointer"
+          className="border-b border-dotted text-[#282828] cursor-pointer"
         >
           &copy; Rostislav Zavodianyi
         </a>
         <button
           onClick={handlePopupToggle}
-          className="border-b border-dotted text-green-800 cursor-pointer"
+          className="border-b border-dotted text-[#282828] cursor-pointer"
         >
           Contact Me
         </button>
@@ -35,20 +35,20 @@ const Footer = () => {
 
       {showSuccessPopup && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center z-60">
-          <div className="bg-green-100 h-8 w-96 flex items-center rounded-t-lg px-4">
+          <div className="bg-[#F3F3F1] h-8 w-96 flex items-center rounded-t-lg px-4">
             <div className="mr-auto flex space-x-1">
-                <button
-                    type="button"
-                    onClick={() => setShowSuccessPopup(false)}
-                    className="text-green-100 hover:text-green-300"
-                >
-                    <div className="w-3 h-3 bg-red-500 rounded-full cursor-pointer"></div>
-                </button>
+              <button
+                type="button"
+                onClick={() => setShowSuccessPopup(false)}
+                className="text-green-100 hover:text-green-300"
+              >
+                <div className="w-3 h-3 bg-red-500 rounded-full cursor-pointer"></div>
+              </button>
             </div>
-            <h4 className="text-green-800 font-medium text-sm">Success</h4>
+            <h4 className="text-[#282828] font-medium text-sm mr-auto">Success</h4>
           </div>
-          <div className="bg-gray-900 border-[1px] border-green-100 w-96 p-6 rounded-b-lg shadow-lg relative">
-            <h2 className="text-lg text-green-100 font-semibold mb-4 text-center">
+          <div className="bg-gray-900 border-[1px] border-[#F3F3F1] w-96 p-6 rounded-b-lg shadow-lg relative">
+            <h2 className="text-lg text-[#F3F3F1] font-semibold mb-4 text-center">
               Thank you! Your submission has been received.
             </h2>
           </div>
@@ -57,20 +57,18 @@ const Footer = () => {
 
       {showPopup && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center z-50">
-          <div className="bg-green-100 h-8 w-96 flex items-center rounded-t-lg px-4">
+          <div className="bg-[#F3F3F1] h-8 w-96 flex items-center rounded-t-lg px-4">
             <div className="mr-auto flex space-x-1">
-                <button
-                    type="button"
-                    onClick={handlePopupToggle}
-                    className="text-green-100 hover:text-green-300"
-                >
-                    <div className="w-3 h-3 bg-red-500 rounded-full cursor-pointer"></div>
-                </button>
+              <button type="button" onClick={handlePopupToggle}>
+                <div className="w-3 h-3 bg-red-500 rounded-full cursor-pointer"></div>
+              </button>
             </div>
-            <h4 className="text-green-800 font-medium text-sm">Contact Form</h4>
+            <h4 className="text-[#282828] font-medium text-sm mr-auto">
+              Contact Form
+            </h4>
           </div>
-          <div className="bg-gray-900 border-[1px] border-green-100 w-96 p-6 rounded-b-lg shadow-lg relative">
-            <h2 className="text-lg text-green-100 font-semibold mb-4 text-center">
+          <div className="bg-gray-900 border-[1px] border-[#F3F3F1] w-96 p-6 rounded-b-lg shadow-lg relative">
+            <h2 className="text-lg text-[#F3F3F1] font-semibold mb-4 text-center">
               Contact Me
             </h2>
             <form
@@ -81,10 +79,11 @@ const Footer = () => {
               className="space-y-4"
             >
               <input type="hidden" name="form-name" value="contact" />
+
               <div>
                 <label
                   htmlFor="name"
-                  className="block text-sm font-medium text-green-100"
+                  className="block text-sm font-medium text-[#F3F3F1]"
                 >
                   Your Name
                 </label>
@@ -93,14 +92,15 @@ const Footer = () => {
                   id="name"
                   name="name"
                   required
-                  className="mt-1 p-2 w-full border placeholder:text-gray-900 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+                  className="mt-1 p-2 w-full border rounded-lg placeholder:text-gray-900 text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500"
                   placeholder="Enter your name"
                 />
               </div>
+
               <div>
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium text-green-100"
+                  className="block text-sm font-medium text-[#F3F3F1]"
                 >
                   Your Email
                 </label>
@@ -109,14 +109,15 @@ const Footer = () => {
                   id="email"
                   name="email"
                   required
-                  className="mt-1 p-2 w-full border placeholder:text-gray-900 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+                  className="mt-1 p-2 w-full border rounded-lg placeholder:text-gray-900 text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500"
                   placeholder="Enter your email"
                 />
               </div>
+
               <div>
                 <label
                   htmlFor="message"
-                  className="block text-sm font-medium text-green-100"
+                  className="block text-sm font-medium text-[#F3F3F1]"
                 >
                   Message
                 </label>
@@ -125,15 +126,15 @@ const Footer = () => {
                   name="message"
                   rows="4"
                   required
-                  className="mt-1 p-2 w-full border placeholder:text-gray-900 text-gray-900 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
+                  className="mt-1 p-2 w-full border rounded-lg placeholder:text-gray-900 text-gray-900 focus:outline-none focus:ring-2 focus:ring-green-500"
                   placeholder="Enter your message"
                 ></textarea>
               </div>
-              <div className="flex justify-between items-center">
+              <div className="flex justify-between">
                 <button
                   type="button"
                   onClick={handlePopupToggle}
-                  className="text-green-100 hover:text-green-300"
+                  className="text-[#F3F3F1] hover:text-green-300"
                 >
                   Cancel
                 </button>
